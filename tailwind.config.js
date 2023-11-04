2f3e4bca67a110124e3b451f8f79d3e6f3179995
Zachary Maillard <zmaillard@protonmail.com>
2023-11-04 08:39:38 -0600

    Pull In Flowbite Javascript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'media',
-  content: ["./layouts/**/*.{html,js}"],
+  content: ["./layouts/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
