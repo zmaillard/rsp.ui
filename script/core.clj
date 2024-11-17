fa7aedeccfe787a2765dcf693a84b355e5499486
Zachary Maillard <zmaillard@healthwise.org>
2024-11-17 09:02:06 -0700

    Updating Sync Scripts

---
+++ script/core.clj
@@ -0,0 +1,40 @@
+(ns core 
+  (:require
+   [babashka.http-client :as http]
+   [cheshire.core :as json]
+   [pod.babashka.postgresql :as pg]))
+
+
+(def base-eagle-url (System/getenv "EAGLE_URL"))
+(def eagle-token (System/getenv "EAGLE_TOKEN"))
+
+(def db {:dbtype "postgresql"
+         :host (System/getenv "DB_HOST")
+         :dbname (System/getenv "DB_NAME")
+         :user (System/getenv "DB_USER")
+         :password (System/getenv "DB_PASSWORD")
+         :port (System/getenv "DB_PORT")})
+
+(defn update-quality
+  [imageid new-quality]
+  (let [imageid-int (biginteger imageid)]
+   (pg/execute! db ["UPDATE sign.highwaysign SET quality = ? WHERE imageid = ?" new-quality imageid-int])))
+  
+(defn get-signs
+  []
+  (pg/execute! db ["select imageid::text as imageid, title, state_name, tagitems, quality from sign.vwindexsign"]))
+
+(defn get-imported-signs
+  []
+  (->
+   (loop [offset 0 signs []]
+     (let [response (http/get (str base-eagle-url "/api/item/list") {:query-params {:limit 200 :offset offset :token eagle-token}})
+           data (json/parse-string (:body response) true)
+           new-signs (concat signs (:data data))]
+       (if (empty? (:data data))
+         new-signs
+         (recur (+ offset 1) new-signs))))))
+
+
+
+
