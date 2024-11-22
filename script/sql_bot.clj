e04a656398d33e39e8d666eae1cd2147476aa2b5
Zach Maillard <zmaillard@protonmail.com>
2024-11-21 20:38:07 -0700

    Add More Fields and View to Bot Database (#126)
    * Add Quality Column
    
    * Building out Views
    
    * Adding View
    
    * Revert
    
    ---------
    
    Co-authored-by: Zachary Maillard <zmaillard@healthwise.org>

--- script/sql_bot.clj
+++ script/sql_bot.clj
@@ -17,18 +17,36 @@
           (take-while #(not= %1 "---"))
           (join "\n")))))
 
+(defn long-string
+  [& strings]
+  (join "\n" strings))
+
+(defn sql-view
+  []
+  (long-string
+     "CREATE VIEW vwSign AS"
+     "SELECT s.imageid, s.title, st.name as state, pl.name as place, ct.name as county, cn.name as country, s.quality"
+     "FROM main.sign as s"
+     "INNER JOIN main.country as cn ON s.country = cn.slug"
+     "INNER JOIN main.state as st ON s.state = st.slug"
+     "LEFT OUTER JOIN main.county as ct ON s.county = ct.key"
+     "LEFT OUTER JOIN main.place as pl ON s.place = pl.key" ))
+
+
 (defn build-db
   [path]
   (fs/delete-if-exists path)
-  (sqlite/execute! path "create table sign (imageid TEXT, title TEXT, state TEXT, place TEXT, county TEXT, country TEXT)")
-  (sqlite/execute! path "create table state (slug TEXT, name TEXT)")
-  (sqlite/execute! path "create table country (slug TEXT, name TEXT)")) 
- 
+  (sqlite/execute! path "create table sign (imageid TEXT, title TEXT, state TEXT, place TEXT, county TEXT, country TEXT, quality INTEGER)")
+  (sqlite/execute! path "create table state (slug TEXT PRIMARY KEY, name TEXT)")
+  (sqlite/execute! path "create table place (slug TEXT, name TEXT, state TEXT, key TEXT PRIMARY KEY)")
+  (sqlite/execute! path "create table county (slug TEXT, name TEXT, state TEXT, key TEXT PRIMARY KEY)")
+  (sqlite/execute! path "create table country (slug TEXT PRIMARY KEY, name TEXT)")
+  (sqlite/execute! path (sql-view)))
 
 (defn load-sign
   [yaml-sign]
-  (let [[imageid title state place county country] ((juxt :imageid :title :state :place :county :country) yaml-sign)]
-    (sqlite/execute! db-path ["INSERT INTO sign (imageid, title, state, place, county, country) VALUES (?,?,?,?,?,?)" imageid title state place county country])))
+  (let [[imageid title state place county country quality] ((juxt :imageid :title :state :place :county :country :quality) yaml-sign)]
+    (sqlite/execute! db-path ["INSERT INTO sign (imageid, title, state, place, county, country, quality) VALUES (?, ?,?,?,?,?,?)" imageid title state place county country quality])))
 
 (defn load-jurisdiction
   [table yaml-sign]
@@ -36,9 +54,19 @@
         insert (str "INSERT INTO " table  "(slug, name) VALUES (?,?)")]
     (sqlite/execute! db-path [insert slug name])))
 
+(defn load-jurisdiction-with-state
+  [table yaml-sign]
+  (let [[slug name state] ((juxt :slug :name :stateslug) yaml-sign)
+        insert (str "INSERT INTO " table  "(slug, name, state, key) VALUES (?,?,?,?)")]
+    (sqlite/execute! db-path [insert slug name state (str state "_" slug)])))
+
 (defn -main
   [& _args]
   (build-db db-path)
+  (doseq [file (fs/glob "content/county" "**/*.md")]
+   (load-jurisdiction-with-state "county" (yaml/parse-string (extract-yaml file))))
+  (doseq [file (fs/glob "content/place" "**/*.md")]
+   (load-jurisdiction-with-state "place" (yaml/parse-string (extract-yaml file))))
   (doseq [file (fs/glob "content/state" "**/*.md")]
    (load-jurisdiction "state" (yaml/parse-string (extract-yaml file))))
   (doseq [file (fs/glob "content/country" "**/*.md")]
