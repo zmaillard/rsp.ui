98b964dd8db554a49244fd88cf744fad6dd333e5
Zachary Maillard <zmaillard@healthwise.org>
2024-12-26 09:19:19 -0700

    Fixed Tag Display

--- pkg/models/tags.go
+++ pkg/models/tags.go
@@ -2,16 +2,40 @@ package models
 
 import (
 	"encoding/json"
+	"strings"
 )
 
 type Tags []Tag
 
 type Tag struct {
-	Id           uint   `gorm:"column:id"`
-	Name         string `gorm:"column:name"`
-	Slug         string `gorm:"column:slug"`
-	IsFlickrOnly bool   `gorm:"column:flickr_only"`
-	IsCategory   bool   `gorm:"column:is_category"`
+	Id           uint   `gorm:"column:id" json:"id"`
+	Name         string `gorm:"column:name" json:"name"`
+	Slug         string `gorm:"column:slug" json:"slug"`
+	IsFlickrOnly bool   `gorm:"column:flickr_only" json:"-"`
+	IsCategory   bool   `gorm:"column:is_category" json:"isCategory"`
+}
+
+func (t *Tag) MarshalJSON() ([]byte, error) {
+	type Alias Tag
+	return json.Marshal(&struct {
+		*Alias
+		Grouper string `json:"grouper"`
+	}{
+		Alias:   (*Alias)(t),
+		Grouper: t.GetGrouper(),
+	})
+}
+
+func (t *Tag) GetGrouper() string {
+	if t.Name == "" {
+		return ""
+	}
+	first := t.Name[0:1]
+	if first >= "0" && first <= "9" {
+		return "0-9"
+	}
+
+	return strings.ToUpper(first)
 }
 
 func (*Tag) TableName() string {
@@ -23,12 +47,5 @@ func (Tags) OutLookupFiles() []string {
 }
 
 func (ts Tags) GetLookup() ([]byte, error) {
-	tags := make(map[string]string)
-
-	for _, v := range ts {
-		tags[v.Slug] = v.Name
-
-	}
-
-	return json.Marshal(tags)
+	return json.Marshal(ts)
 }
