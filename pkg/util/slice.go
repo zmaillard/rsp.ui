cf91c33ae65cf80b39e8220a904c0cbbc46a92a0
Zachary Maillard <zmaillard@protonmail.com>
2025-09-03 18:54:37 -0600

    Adding Slice Util + Hiding Empty State Subs

--- pkg/util/slice.go
+++ pkg/util/slice.go
@@ -9,3 +9,15 @@ func SliceMap[T any, M any](i []T, f func(highway T) M) []M {
 
 	return o
 }
+
+func SliceFilter[T any](i []T, f func(highway T) bool) []T {
+	o := make([]T, 0)
+
+	for idx, _ := range i {
+		if f(i[idx]) {
+			o = append(o, i[idx])
+		}
+	}
+
+	return o
+}
