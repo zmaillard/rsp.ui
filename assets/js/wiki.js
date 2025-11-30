0eb7af0c2b32e17ab245c148410b69c1864eb7ea
Zachary Maillard <zmaillard@protonmail.com>
2025-11-30 08:10:06 -0700

    Wiki Text for Highways

---
+++ assets/js/wiki.js
@@ -0,0 +1,32 @@
+(function() {
+    let domWikiSearch = document.getElementById('wiki-search');
+    if (!domWikiSearch) {
+        return;
+    }
+    let externalPageLink =  domWikiSearch.dataset.externalLink;  "{{ $externalLink }}";
+    if (!externalPageLink) {
+        return;
+    }
+    if (!("parse" in URL)) {
+        return;
+    }
+
+    let path = URL.parse(externalPageLink).pathname;
+    let title = path.substring(path.lastIndexOf('/') + 1);
+    if (!title) return;
+    let wikiApiUrl = `https://en.wikipedia.org/w/api.php?action=query&redirects=1&explaintext=true&exintro=true&prop=extracts&titles=${title}&format=json&origin=*`;
+
+    fetch(wikiApiUrl, {method: "GET", headers: { "Api-User-Agent": "admin@roadsign.pictures" }})
+        .then(f=> f.json())
+        .then(f=>{
+            let keys = Object.keys(f.query.pages);
+            if (keys.length > 0) {
+                let pageId = keys[0];
+                let text = f.query.pages[pageId].extract;
+
+                if (text) {
+                    domWikiSearch.innerHTML = `${text} Source: <a class="hover:underline  inline-flex" rel="noopener noreferrer" target='_blank' href='${externalPageLink}'>Wikipedia</a>`;
+                }
+            }
+        });
+})();
